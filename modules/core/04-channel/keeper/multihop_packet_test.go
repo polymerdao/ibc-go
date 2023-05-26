6df39838ac925e496aa4fec0a09e536326474980
George Verbitsky <george@polymerlabs.org>
2023-05-25 18:06:13 -0700

    ibc-16: RecvPacket test cases ported to multihop test (one test case needs to be debugged)

--- modules/core/04-channel/keeper/multihop_packet_test.go
+++ modules/core/04-channel/keeper/multihop_packet_test.go
@@ -1,9 +1,14 @@
 package keeper_test
 
 import (
+	"errors"
 	"fmt"
 
+	sdkerrors "cosmossdk.io/errors"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+
+	clienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
+	connectiontypes "github.com/cosmos/ibc-go/v7/modules/core/03-connection/types"
 	"github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 	"github.com/cosmos/ibc-go/v7/modules/core/exported"
 	ibctesting "github.com/cosmos/ibc-go/v7/testing"
@@ -25,11 +30,13 @@ func (suite *MultihopTestSuite) TestRecvPacket() {
 		packet       *types.Packet
 		packetHeight exported.Height
 		channelCap   *capabilitytypes.Capability
+		expError     *sdkerrors.Error
 		err          error
 	)
 
 	testCases := []channelTestCase{
 		{"success: ORDERED channel", true, func() {
+			suite.SetupChannels()
 			packet, packetHeight, err = suite.A().
 				SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
 			suite.Require().NoError(err)
@@ -38,6 +45,7 @@ func (suite *MultihopTestSuite) TestRecvPacket() {
 			)
 		}, true},
 		{"success: UNORDERED channel", true, func() {
+			suite.SetupChannels()
 			packet, packetHeight, err = suite.A().
 				SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
 			suite.Require().NoError(err)
@@ -45,19 +53,249 @@ func (suite *MultihopTestSuite) TestRecvPacket() {
 				suite.Z().ChannelConfig.PortID, suite.Z().ChannelID,
 			)
 		}, true},
-		// {"success with out order packet: UNORDERED channel", false,  func() {
+		{"success with out of order packet: UNORDERED channel", false, func() {
+			// setup uses an UNORDERED channel
+			suite.SetupChannels()
+			// send 2 packets
+			packet, packetHeight, err = suite.A().
+				SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
+			suite.Require().NoError(err)
+			packet, packetHeight, err = suite.A().
+				SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
+			suite.Require().NoError(err)
+			channelCap = suite.Z().Chain.GetChannelCapability(
+				suite.Z().ChannelConfig.PortID, suite.Z().ChannelID,
+			)
+		}, true},
+		{"packet already relayed ORDERED channel (no-op)", false, func() {
+			expError = types.ErrNoOpMsg
 
-		// }, true},
+			suite.chanPath.SetChannelOrdered()
+			suite.SetupChannels()
+
+			packet, packetHeight, err = suite.A().
+				SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
+			suite.Require().NoError(err)
+			channelCap = suite.Z().Chain.GetChannelCapability(
+				suite.Z().ChannelConfig.PortID, suite.Z().ChannelID,
+			)
+
+			err = suite.Z().RecvPacket(packet, packetHeight)
+			suite.Require().NoError(err)
+		}, false},
+		{"packet already relayed UNORDERED channel (no-op)", false, func() {
+			expError = types.ErrNoOpMsg
+
+			// setup uses an UNORDERED channel
+			suite.SetupChannels()
+
+			packet, packetHeight, err = suite.A().
+				SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
+			suite.Require().NoError(err)
+			channelCap = suite.Z().Chain.GetChannelCapability(
+				suite.Z().ChannelConfig.PortID, suite.Z().ChannelID,
+			)
+
+			err = suite.Z().RecvPacket(packet, packetHeight)
+			suite.Require().NoError(err)
+		}, false},
+		{"out of order packet failure with ORDERED channel", true, func() {
+			expError = types.ErrPacketSequenceOutOfOrder
+
+			suite.SetupChannels()
+
+			// send 2 packets
+			packet, packetHeight, err = suite.A().
+				SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
+			suite.Require().NoError(err)
+			packet, packetHeight, err = suite.A().
+				SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
+			suite.Require().NoError(err)
+			channelCap = suite.Z().Chain.GetChannelCapability(
+				suite.Z().ChannelConfig.PortID, suite.Z().ChannelID,
+			)
+		}, false},
+		{"channel not found", false, func() {
+			expError = types.ErrChannelNotFound
+
+			// use wrong channel naming
+			suite.SetupChannels()
+			*packet = types.NewPacket(ibctesting.MockPacketData, 1, suite.A().ChannelConfig.PortID, suite.A().ChannelID, ibctesting.InvalidID, ibctesting.InvalidID, defaultTimeoutHeight, disabledTimeoutTimestamp)
+			packetHeight = suite.A().Chain.LastHeader.GetHeight()
+			channelCap = suite.Z().Chain.GetChannelCapability(
+				suite.Z().ChannelConfig.PortID, suite.Z().ChannelID,
+			)
+		}, false},
+		{"channel not open", false, func() {
+			expError = types.ErrInvalidChannelState
+
+			suite.SetupChannels()
+			packet, packetHeight, err = suite.A().
+				SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
+
+			err = suite.Z().SetChannelState(types.CLOSED)
+			suite.Require().NoError(err)
+			channelCap = suite.Z().Chain.GetChannelCapability(
+				suite.Z().ChannelConfig.PortID, suite.Z().ChannelID,
+			)
+		}, false},
+		{"capability cannot authenticate ORDERED", true, func() {
+			expError = types.ErrInvalidChannelCapability
+
+			suite.SetupChannels()
+
+			packet, packetHeight, err = suite.A().
+				SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
+
+			channelCap = capabilitytypes.NewCapability(3)
+		}, false},
+		{"packet source port ≠ channel counterparty port", false, func() {
+			expError = types.ErrInvalidPacket
+
+			suite.SetupChannels()
+
+			*packet = types.NewPacket(ibctesting.MockPacketData, 1, ibctesting.InvalidID, suite.A().ChannelID, suite.Z().ChannelConfig.PortID, suite.Z().ChannelID, defaultTimeoutHeight, disabledTimeoutTimestamp)
+			packetHeight = suite.A().Chain.LastHeader.GetHeight()
+
+			channelCap = suite.Z().Chain.GetChannelCapability(
+				suite.Z().ChannelConfig.PortID, suite.Z().ChannelID,
+			)
+		}, false},
+		{"packet source channel ID ≠ channel counterparty channel ID", false, func() {
+			expError = types.ErrInvalidPacket
+
+			suite.SetupChannels()
+
+			*packet = types.NewPacket(ibctesting.MockPacketData, 1, suite.A().ChannelConfig.PortID, ibctesting.InvalidID, suite.Z().ChannelConfig.PortID, suite.Z().ChannelID, defaultTimeoutHeight, disabledTimeoutTimestamp)
+			packetHeight = suite.A().Chain.LastHeader.GetHeight()
+
+			channelCap = suite.Z().Chain.GetChannelCapability(
+				suite.Z().ChannelConfig.PortID, suite.Z().ChannelID,
+			)
+		}, false},
+		{"connection not found", false, func() {
+			expError = connectiontypes.ErrConnectionNotFound
+
+			suite.SetupChannels()
+
+			// pass channel check
+			channel := types.NewChannel(types.OPEN, types.ORDERED, types.NewCounterparty(suite.A().ChannelConfig.PortID, suite.A().ChannelID), []string{connIDB}, suite.Z().ChannelConfig.Version)
+			suite.Z().Chain.App.GetIBCKeeper().ChannelKeeper.SetChannel(suite.Z().Chain.GetContext(), suite.Z().ChannelConfig.PortID, suite.Z().ChannelID, channel)
+
+			packet, packetHeight, err = suite.A().
+				SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
+
+			suite.Z().Chain.CreateChannelCapability(suite.Z().Chain.GetSimApp().ScopedIBCMockKeeper, suite.Z().ChannelConfig.PortID, suite.Z().ChannelID)
+			channelCap = suite.Z().Chain.GetChannelCapability(suite.Z().ChannelConfig.PortID, suite.Z().ChannelID)
+		}, false},
+		{"connection not OPEN", false, func() {
+			expError = connectiontypes.ErrInvalidConnectionState
+
+			suite.coord.SetupClients(suite.chanPath)
+
+			// connection on chainZ is in INIT
+			err = suite.Z().ConnOpenInit()
+			suite.Require().NoError(err)
+
+			// pass channel check
+			channel := types.NewChannel(types.OPEN, types.ORDERED, types.NewCounterparty(suite.A().ChannelConfig.PortID, suite.A().ChannelID), []string{suite.Z().ConnectionID}, suite.Z().ChannelConfig.Version)
+			suite.Z().Chain.App.GetIBCKeeper().ChannelKeeper.SetChannel(suite.Z().Chain.GetContext(), suite.Z().ChannelConfig.PortID, suite.Z().ChannelID, channel)
+			*packet = types.NewPacket(ibctesting.MockPacketData, 1, suite.A().ChannelConfig.PortID, suite.A().ChannelID, suite.Z().ChannelConfig.PortID, suite.Z().ChannelID, defaultTimeoutHeight, disabledTimeoutTimestamp)
+			// packetHeight = suite.A().Chain.LastHeader.GetHeight()
+			// fmt.Printf("packetHeight = %v\n", packetHeight)
+			packetHeight = suite.Z().Chain.LastHeader.GetHeight()
+			// fmt.Printf("packetHeight = %v\n", packetHeight)
+
+			suite.Z().Chain.CreateChannelCapability(suite.Z().Chain.GetSimApp().ScopedIBCMockKeeper, suite.Z().ChannelConfig.PortID, suite.Z().ChannelID)
+			channelCap = suite.Z().Chain.GetChannelCapability(suite.Z().ChannelConfig.PortID, suite.Z().ChannelID)
+		}, false},
+		{"timeout height passed", false, func() {
+			expError = types.ErrPacketTimeout
+
+			suite.SetupChannels()
+
+			*packet = types.NewPacket(ibctesting.MockPacketData, 1, suite.A().ChannelConfig.PortID, suite.A().ChannelID, suite.Z().ChannelConfig.PortID, suite.Z().ChannelID, clienttypes.GetSelfHeight(suite.Z().Chain.GetContext()), disabledTimeoutTimestamp)
+			packetHeight = suite.A().Chain.LastHeader.GetHeight()
+
+			channelCap = suite.Z().Chain.GetChannelCapability(
+				suite.Z().ChannelConfig.PortID, suite.Z().ChannelID,
+			)
+		}, false},
+		{"timeout timestamp passed", false, func() {
+			expError = types.ErrPacketTimeout
+
+			suite.SetupChannels()
+
+			*packet = types.NewPacket(ibctesting.MockPacketData, 1, suite.A().ChannelConfig.PortID, suite.A().ChannelID, suite.Z().ChannelConfig.PortID, suite.Z().ChannelID, disabledTimeoutHeight, uint64(suite.Z().Chain.GetContext().BlockTime().UnixNano()))
+			packetHeight = suite.A().Chain.LastHeader.GetHeight()
+
+			channelCap = suite.Z().Chain.GetChannelCapability(
+				suite.Z().ChannelConfig.PortID, suite.Z().ChannelID,
+			)
+		}, false},
+/*
+		{"next receive sequence is not found", false, func() {
+			expError = types.ErrSequenceReceiveNotFound
+
+			suite.SetupConnections()
+
+			suite.A().ChannelID = ibctesting.FirstChannelID
+			suite.Z().ChannelID = ibctesting.FirstChannelID
+
+			// manually creating channel prevents next recv sequence from being set
+			channel := types.NewChannel(types.OPEN, types.ORDERED, types.NewCounterparty(suite.A().ChannelConfig.PortID, suite.A().ChannelID), []string{suite.Z().ConnectionID}, suite.Z().ChannelConfig.Version)
+			suite.Z().Chain.App.GetIBCKeeper().ChannelKeeper.SetChannel(suite.Z().Chain.GetContext(), suite.Z().ChannelConfig.PortID, suite.Z().ChannelID, channel)
+
+			*packet = types.NewPacket(ibctesting.MockPacketData, 1, suite.A().ChannelConfig.PortID, suite.A().ChannelID, suite.Z().ChannelConfig.PortID, suite.Z().ChannelID, defaultTimeoutHeight, disabledTimeoutTimestamp)
+
+			// manually set packet commitment
+			suite.A().Chain.App.GetIBCKeeper().ChannelKeeper.SetPacketCommitment(suite.A().Chain.GetContext(), suite.A().ChannelConfig.PortID, suite.A().ChannelID, packet.GetSequence(), types.CommitPacket(suite.A().Chain.App.AppCodec(), packet))
+			packetHeight = suite.A().Chain.LastHeader.GetHeight()
+
+			suite.Z().Chain.CreateChannelCapability(suite.Z().Chain.GetSimApp().ScopedIBCMockKeeper, suite.Z().ChannelConfig.PortID, suite.Z().ChannelID)
+			channelCap = suite.Z().Chain.GetChannelCapability(suite.Z().ChannelConfig.PortID, suite.Z().ChannelID)
+
+			err = suite.A().UpdateClient()
+			suite.Require().NoError(err)
+			err = suite.Z().UpdateClient()
+			suite.Require().NoError(err)
+		}, false},
+*/
+		{"receipt already stored", false, func() {
+			expError = types.ErrNoOpMsg
+			suite.SetupChannels()
+
+			packet, packetHeight, err = suite.A().
+				SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
+			suite.Require().NoError(err)
+
+			sequence := packet.GetSequence()
+			suite.Z().Chain.App.GetIBCKeeper().ChannelKeeper.SetPacketReceipt(suite.Z().Chain.GetContext(), suite.Z().ChannelConfig.PortID, suite.Z().ChannelID, sequence)
+			channelCap = suite.Z().Chain.GetChannelCapability(suite.Z().ChannelConfig.PortID, suite.Z().ChannelID)
+		}, false},
+		{"validation failed", false, func() {
+			// skip error code check, downstream error code is used from light-client implementations
+
+			// packet commitment not set resulting in invalid proof
+			suite.SetupChannels()
+
+			*packet = types.NewPacket(ibctesting.MockPacketData, 1, suite.A().ChannelConfig.PortID, suite.A().ChannelID, suite.Z().ChannelConfig.PortID, suite.Z().ChannelID, defaultTimeoutHeight, disabledTimeoutTimestamp)
+			packetHeight = suite.A().Chain.LastHeader.GetHeight()
+
+			channelCap = suite.Z().Chain.GetChannelCapability(suite.Z().ChannelConfig.PortID, suite.Z().ChannelID)
+		}, false},
 	}
+
+	packet = &types.Packet{}
 
 	for _, tc := range testCases {
 		tc := tc
 		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
 			suite.SetupTest() // reset
+			expError = nil    // must explicitly set for failed cases
 			if tc.orderedChannel {
 				suite.chanPath.SetChannelOrdered()
 			}
-			suite.SetupChannels() // setup multihop channels
 
 			tc.malleate()
 
@@ -103,13 +341,19 @@ func (suite *MultihopTestSuite) TestRecvPacket() {
 					suite.Require().False(receiptStored, "packet receipt stored in ORDERED channel")
 				} else {
 					suite.Require().True(channelZ.Ordering == types.UNORDERED)
-					suite.Require().Equal(nextSeqRecv, packet.GetSequence(), "sequence incremented in UNORDERED channel")
-					suite.Require().Equal(nextSeqRecv, uint64(2), "sequence incremented in UNORDERED channel")
+					// suite.Require().Equal(nextSeqRecv, packet.GetSequence(), "sequence incremented in UNORDERED channel")
+					suite.Require().Equal(uint64(1), nextSeqRecv, "sequence incremented in UNORDERED channel")
 					suite.Require().True(receiptStored, "packet receipt not stored in UNORDERED channel")
 					suite.Require().Equal(string([]byte{byte(1)}), receipt, "packet receipt not stored in UNORDERED channel")
 				}
 			} else {
 				suite.Require().Error(err)
+				// fmt.Printf("err = %v\n", err)
+
+				// only check if expError is set, since not all error codes can be known
+				if expError != nil {
+					suite.Require().True(errors.Is(err, expError))
+				}
 			}
 		})
 	}
